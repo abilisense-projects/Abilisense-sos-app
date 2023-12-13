1df770ea7194fc87fa40e8db7dcce4fdd4b10b50
mirisinger <hand1b14@mbjcomp.org.il>
2023-12-13 11:14:15 +0200

    Add SignUp

--- panic-button/client/App.js
+++ panic-button/client/App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+
+import SignUpPage from './pages/SignUpPage';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
-
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+     <Stack.Navigator>
+      <Stack.Screen name="SignUpPage" component={SignUpPage} />
+     </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -19,26 +25,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
