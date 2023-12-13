98b181a707ca88a9ac637bdd774421947e57cb83
yehuditshulevits <y6764634@gmail.com>
2023-12-13 14:05:50 +0200

    login

--- panic-button/client/App.js
+++ panic-button/client/App.js
@@ -2,14 +2,11 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './pages/Login';
-import store from './redux/store';
-import { Provider } from 'react-redux';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={Login} />
@@ -17,7 +14,6 @@ const App = () => {
           {/* <Stack.Screen name="Register" component={Register} /> */}
         </Stack.Navigator>
       </NavigationContainer>
-    </Provider>
   );
 };
 export default App;
