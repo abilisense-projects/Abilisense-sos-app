4c0aed3df49c6a49bce180b3864dc0281ab5e0a3
yehuditshulevits <y6764634@gmail.com>
2023-12-13 11:48:20 +0200

    fix

--- panic-button/client/Redux/actions.js
+++
@@ -1,8 +0,0 @@
-// import { Action } from 'redux';
-
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-
-export const loginSuccess = (username) => ({
-  type: LOGIN_SUCCESS,
-  payload: { username },
-});
--- panic-button/client/pages/Login.js
+++ panic-button/client/pages/Login.js
@@ -1,17 +1,107 @@
+// import React, { useState } from 'react';
+// import { View, Text, TextInput, Button, StyleSheet, AsyncStorage } from 'react-native';
+// import * as Yup from 'yup';
+// import axios from 'axios';
+// // import {AsyncStorage} from '@react-native-async-storage/async-storage';
+// // import jwt_decode from 'jwt-decode';
+
+// const LoginSchema = Yup.object().shape({
+//   email: Yup.string().email('Invalid email format').required('Email is required'),
+//   password: Yup.string()
+//     .min(6, 'Password must be at least 6 characters')
+//     .matches(
+//       /^(?=.*[0-9])(?=.*[a-zA-Z])/,
+//       'Password must include at least one letter and one number'
+//     )
+//     .required('Password is required'),
+// });
+
+// const Login = ({ navigation }) => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [errors, setErrors] = useState({});
+
+//   const handleLogin = async () => {
+//     try {
+//       await LoginSchema.validate({ email, password }, { abortEarly: false });
+
+//       // Make a POST request to your Node.js backend for user authentication- need the code from node
+//       const response = await axios.post('YOUR_LOGIN_ENDPOINT', {
+//         email,
+//         password,
+//       });
+//       const { token } = response.data; // Assuming server responds with a token upon successful login
+
+//       if (token) {
+//         // Save the token securely (you can use AsyncStorage for this)
+//         await AsyncStorage.setItem('token', token);
+
+//         // Decode the token to extract user data if needed
+//         const decodedToken = jwt_decode(token);
+//         console.log(decodedToken);
+
+
+//         // Assuming your backend responds with a success message upon successful authentication
+//         if (response.data && response.data.success) {
+//           console.log('Login successful');
+//           navigation.navigate('Home');
+//         } else {
+//           console.error('Invalid credentials');
+//         }}
+//       } catch (error) {
+//         if (error instanceof Yup.ValidationError) {
+//           const yupErrors = {};
+//           error.inner.forEach((e) => {
+//             yupErrors[e.path] = e.message;
+//           });
+//           setErrors(yupErrors);
+//         }
+//         console.error(error.message);
+//       }
+//     };
+
+//     return (
+//       <View style={styles.container}>
+//         <View style={styles.registerContainer}>
+//           <Button title="Register" style={styles.register} onPress={() => navigation.navigate('Register')} />
+//         </View>
+//         <Text style={styles.header}>Login</Text>
+//         <TextInput
+//           style={[styles.input, errors.email && styles.invalidInput]}
+//           placeholder="Email"
+//           onChangeText={(text) => {
+//             setEmail(text);
+//             setErrors({ ...errors, email: '' });
+//           }}
+//           value={email}
+//         />
+//         {errors.email && <Text style={styles.warningText}>{errors.email}</Text>}
+
+//         <TextInput
+//           style={[styles.input, errors.password && styles.invalidInput]}
+//           placeholder="Password"
+//           secureTextEntry
+//           onChangeText={(text) => {
+//             setPassword(text);
+//             setErrors({ ...errors, password: '' });
+//           }}
+//           value={password}
+//         />
+//         {errors.password && <Text style={styles.warningText}>{errors.password}</Text>}
+
+//         <Text style={styles.forgotPassword} onPress={() => navigation.navigate('ForgotPassword')}>
+//           Forgot Password?
+//         </Text>
+//         <Button title="Login" onPress={handleLogin} />
+//       </View>
+//     );
+//   };
+
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, AsyncStorage } from 'react-native';
-import * as Yup from 'yup';
-
-const LoginSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email format').required('Email is required'),
-  password: Yup.string()
-    .min(6, 'Password must be at least 6 characters')
-    .matches(
-      /^(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>])/,
-      'Password must include at least one special character and one number'
-    )
-    .required('Password is required'),
-});
+import axios from 'axios';
+import { loginValidationSchema } from '../config/ValidationSchemas';
+import { Yup } from '../config/ValidationSchemas';
 
 const Login = ({ navigation }) => {
   const [email, setEmail] = useState('');
@@ -20,27 +110,27 @@ const Login = ({ navigation }) => {
 
   const handleLogin = async () => {
     try {
-      await LoginSchema.validate({ email, password }, { abortEarly: false });
+      await loginValidationSchema.validate({ email, password }, { abortEarly: false });
 
-      // Connect to MongoDB and verify user credentials
-      // Replace the next lines with the actual logic for connecting to MongoDB
-      // const user = await usersCollection.findOne({ email, password });
-      const user = { username: 'test' }; // Example user object
-      if (!user) {
-        console.error('Invalid credentials');
-        return;
+      // Make a POST request to your Node.js backend for user authentication- need the code from node
+      const response = await axios.post('YOUR_LOGIN_ENDPOINT', {
+        email,
+        password,
+      });
+      const { token } = response.data; // Assuming server responds with a token upon successful login
+
+      if (token) {
+        // Save the token securely (you can use AsyncStorage for this)
+        await AsyncStorage.setItem('token', token);
+
+        // Assuming your backend responds with a success message upon successful authentication
+        if (response.data && response.data.success) {
+          console.log('Login successful');
+          navigation.navigate('Home');
+        } else {
+          console.error('Invalid credentials');
+        }
       }
-
-      console.log(user);
-      // Create JWT token
-      // Replace the next line with the actual logic for creating a JWT token
-      // const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
-      const token = 'example_token'; // Example token
-      // Store token in local storage and navigate to the home screen
-      // Replace the next line with the actual logic for storing the token
-      // await AsyncStorage.setItem('token', token);
-      console.log('Login successful');
-      navigation.navigate('Home');
     } catch (error) {
       if (error instanceof Yup.ValidationError) {
         const yupErrors = {};
@@ -55,9 +145,12 @@ const Login = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-        <Button title="Register" style={styles.register} onPress={() => navigation.navigate('Register')}>
-        </Button>
+      <View style={styles.registerContainer}>
+        <Button
+          title="Register"
+          style={styles.register}
+          onPress={() => navigation.navigate('Register')}
+        />
       </View>
       <Text style={styles.header}>Login</Text>
       <TextInput
@@ -70,7 +163,7 @@ const Login = ({ navigation }) => {
         value={email}
       />
       {errors.email && <Text style={styles.warningText}>{errors.email}</Text>}
-
+  
       <TextInput
         style={[styles.input, errors.password && styles.invalidInput]}
         placeholder="Password"
@@ -82,15 +175,18 @@ const Login = ({ navigation }) => {
         value={password}
       />
       {errors.password && <Text style={styles.warningText}>{errors.password}</Text>}
-
-      <Text style={styles.forgotPassword} onPress={() => navigation.navigate('ForgotPassword')}>
+  
+      <Text
+        style={styles.forgotPassword}
+        onPress={() => navigation.navigate('ForgotPassword')}
+      >
         Forgot Password?
       </Text>
       <Button title="Login" onPress={handleLogin} />
     </View>
   );
+  
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -102,7 +198,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   input: {
-    width: '75%',
+    width: '25%',
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
@@ -126,8 +222,12 @@ const styles = StyleSheet.create({
   register: {
     fontSize: 16,
     color: 'blue',
-    flex: 1,
-    justifyContent: 'flex-start',
+  },
+  registerContainer: {
+    position: 'absolute',
+    left: 10,
+    top: 10,
+    zIndex: 1,
   },
 });
 
