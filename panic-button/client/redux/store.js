2b9f5cd36b0041ede4032f9816d88300de084443
yehuditshulevits <y6764634@gmail.com>
2023-12-26 15:19:13 +0200

    fix the bug with redux

--- panic-button/client/redux/store.js
+++ panic-button/client/redux/store.js
@@ -1,10 +1,37 @@
+// import { createStore, combineReducers } from 'redux';
+// import registerReducers from './reducers/registerReducers';
+// import userReducer from './reducers/loginReducers';
+
+// const combinedReducers = combineReducers({
+//   userReducer: userReducer,
+//   register: registerReducers,
+// });
+// const store = createStore(combinedReducers);
+// export default store;
+
 import { createStore, combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Defaults to localStorage for web
+
 import registerReducers from './reducers/registerReducers';
 import userReducer from './reducers/loginReducers';
 
-const combinedReducers = combineReducers({
+const rootReducer = combineReducers({
   userReducer: userReducer,
   register: registerReducers,
 });
-const store = createStore(combinedReducers);
-export default store;
+
+// Configuration for redux-persist
+const persistConfig = {
+  key: 'root', // Key for the persistor
+  storage, // This is your storage backend (localStorage by default)
+  // Optionally, you can whitelist specific reducers to persist
+  // whitelist: ['userReducer'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
